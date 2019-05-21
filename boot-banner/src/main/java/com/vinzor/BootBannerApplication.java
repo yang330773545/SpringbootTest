3b17475c306d9cb4edb3e306cdd0d11d8f4a4606
yangyang <yangyang@vinzor.com>
2019-05-21 21:17:00 +0800

    banner是真滴好呀

---
+++ boot-banner/src/main/java/com/vinzor/BootBannerApplication.java
@@ -0,0 +1,15 @@
+package com.vinzor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//先图后txt 
+//http://www.network-science.de/ascii/
+@SpringBootApplication
+public class BootBannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootBannerApplication.class, args);
+	}
+
+}
