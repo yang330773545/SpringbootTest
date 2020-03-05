9baf1d02a66083b9b9326e00f9dbc47aa978878d
yangyang <yangyang@vinzor.com>
2020-03-05 16:55:22 +0800

    # elasticsearch加强

--- elasticsearch/src/main/java/com/vinzor/po/Teacher.java
+++ elasticsearch/src/main/java/com/vinzor/po/Teacher.java
@@ -3,13 +3,16 @@ package com.vinzor.po;
 import java.io.Serializable;
 
 import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
 
 // /teacher/mytest  这里的索引等同于关系型数据库中数据表 type主要用于分组
 @Document(indexName = "teacher",type="mytest")
 public class Teacher implements Serializable{
-
-
+	private static final long serialVersionUID = 123123123123213L;
 	private int id;
+	//  type自动检测属性的类型，可以根据实际情况自己设置
+	@Field(type = FieldType.Keyword)
 	private String name;
 	private int age;
 	private String school;
